edc315738962d206b08942f05add33891443fa50
saurav <saurabh.shr99@gmail.com>
2026-03-18 14:32:38 +0530

    learn about api basics and routes handling

--- src/app.js
+++ src/app.js
@@ -2,6 +2,24 @@ import express from "express";
 
 const app = express();
 
+app.get("/user/:id", (req, res) => {
+  const { id } = req.params;
+  const { age, name } = req.query;
+  res.send({
+    name,
+    age,
+    id,
+  });
+});
+
+app.post("/user", (req, res) => {
+  res.send("user added successfully");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("user deleted successfully");
+});
+
 app.use("/", (req, res) => {
   res.send("Hi from server");
 });
